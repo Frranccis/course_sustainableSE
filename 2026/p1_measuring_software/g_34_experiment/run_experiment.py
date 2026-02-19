960ffd8e4a98745d4e1ecbed8b6c1c88b079208f
Ada Turgut <a.turgut@student.tudelft.nl>
2026-02-19 11:08:58 +0100

    initial automation

--- 2026/p1_measuring_software/g_34_experiment/run_experiment.py
+++ 2026/p1_measuring_software/g_34_experiment/run_experiment.py
@@ -1,15 +1,38 @@
 
 import asyncio
 import argparse
+import random
 from playwright.async_api import async_playwright
 
 VIDEO_URL = "https://www.youtube.com/watch?v=cdKop6aixVE"  # up to 4k, no ads
 EXPERIMENT_DURATION = 10 # how long the video should play in the experiment (in seconds) # TODO - set a real duration
+QUALITIES = [480, 2160]
+
 
 """
+Under target/release add the energibridge executable (cargo build -- release) so we can call the command
 example run:
-python run_experiment.py --quality 480 --output ./results/q480/run_01.csv
+python run_experiment.py --qualities 480 2160
 """
+
+
+async def run_all_iterations():
+    # represent each iteration with a list entry
+    quality_list = []
+    for q in QUALITIES:
+        quality_list.extend([q] * 2) # this is only 2 for testing, should be 30
+    print("qualities are" + str(quality_list))
+
+    # randomly choose between the two cases
+    random.shuffle(quality_list)
+
+    for i, quality in enumerate(quality_list, start=1):
+        output_file = f"./results/q{quality}/run_{i:02d}.csv"
+        print(f"\n=== Run {i} | Quality {quality}p ===")
+        await run_experiment(quality, output_file)
+        print(f"\n=== Finished, sleeping in between")
+        await asyncio.sleep(10000)
+
 
 async def run_experiment(quality, output_file):
     playwright = await async_playwright().start()
@@ -78,7 +101,9 @@ async def run_experiment(quality, output_file):
     # measurement
     print(f"Measuring {EXPERIMENT_DURATION}s at {quality}p...")
 
-    # TODO - start measurements with EnergiBridge
+    # start measurement with release
+    await start_energibridge(output_file)
+
     await page.wait_for_timeout(EXPERIMENT_DURATION * 1000)
 
     print("Measurement done.")
@@ -90,9 +115,24 @@ async def run_experiment(quality, output_file):
     print("Browser closed")
 
 
+async def start_energibridge(output):
+    print("Starting energibridge.")
+    process = await asyncio.create_subprocess_exec(
+        "target/release/energibridge",
+        "-o", output,
+        "--summary",
+        "sleep", str(EXPERIMENT_DURATION)
+    )
+    await process.wait()
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--quality", required=True) # f.e. 480 or 2160
-    parser.add_argument("--output", required=True) # f.e. ./results/q480/run_01.csv
+    parser.add_argument("--qualities", nargs="+", type=int, required=True) # f.e. 480 2160
+
+    # old output file argument, currently not used
+    parser.add_argument("--output", required=False) # f.e. ./results/q480/run_01.csv
+
     args = parser.parse_args()
-    asyncio.run(run_experiment(args.quality, args.output))
+    QUALITIES = args.qualities
+    asyncio.run(run_all_iterations())
